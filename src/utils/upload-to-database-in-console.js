6b2b6d445a6f94e6d34ed09651628dd8616f6f59
Gus <regustration@gmail.com>
2019-09-24 22:44:02 +0800

    refactor: polish upload-in-console util

--- src/utils/upload-to-database-in-console.js
+++ src/utils/upload-to-database-in-console.js
@@ -1,26 +1,31 @@
 // import jQuery
-var script = document.createElement('script')
+const script = document.createElement('script')
+script.onload = onLoad
 script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'
 document.getElementsByTagName('head')[0].appendChild(script)
 
-let apiDatas = JSON.parse('DATA GOES HERE')
+function onLoad () {
+  window.gogo = async jsonString => {
+    if (!jsonString) return
+    let apiDatas = JSON.parse(jsonString)
 
-const upload = data =>
-  $.get(
-    'http://tad.froghome.org/upload/AddDetailDataGo.php',
-    {
-      ...data,
-      behavior_list_text: '0;;;1. 無:::1;;;2. 聚集:::2;;;3. 鳴叫:::3;;;4. 築巢:::4;;;5. 領域:::5;;;6. 配對:::6;;;7. 打架:::7;;;8. 護幼:::8;;;9. 單獨:::9;;;10. 覓食:::10;;;11. 休息:::11;;;12. 屍體:::12;;;13. 畸形蛙:::13;;;14. 病蛙',
-      living_type_list_text: '1;;;1. 卵塊:::2;;;2. 蝌蚪:::3;;;3. 幼體:::4;;;4. 雄蛙:::5;;;5. 雌蛙:::8;;;6. 成蛙'
-    }
-  )
+    const upload = data =>
+      $.get(
+        'http://tad.froghome.org/upload/AddDetailDataGo.php',
+        {
+          ...data,
+          behavior_list_text: '0;;;1. 無:::1;;;2. 聚集:::2;;;3. 鳴叫:::3;;;4. 築巢:::4;;;5. 領域:::5;;;6. 配對:::6;;;7. 打架:::7;;;8. 護幼:::8;;;9. 單獨:::9;;;10. 覓食:::10;;;11. 休息:::11;;;12. 屍體:::12;;;13. 畸形蛙:::13;;;14. 病蛙',
+          living_type_list_text: '1;;;1. 卵塊:::2;;;2. 蝌蚪:::3;;;3. 幼體:::4;;;4. 雄蛙:::5;;;5. 雌蛙:::8;;;6. 成蛙'
+        }
+      )
 
-const gogo = _ => Promise.all(apiDatas.map(upload))
-  .then(res => {
+    await Promise.all(apiDatas.map(upload))
+
     $('iframe[name="main_show"]')[0]
       .contentDocument
       .querySelectorAll('frame[name="main_top"]')[0]
       .contentDocument
       .location
       .reload()
-  })
+  }
+}
